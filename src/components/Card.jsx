bcd9000b8fe02bdd5ea3ca8551bd1ec9ef9e1008
sana <sana.nasani3@gmail.com>
2024-03-08 20:10:41 +0300

    added sold out badge

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
+  console.log(props.openSpots);
   return (
     <div className="card">
+      {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
       <img
-        src="/images/katie-zaferes.png"
+        src={`/images/${props.img}`}
         alt="Image of Katie Zaferes."
         className="card--image"
       />
       <div className="card--stats">
         <img src="/images/star.png" alt="Star icon" className="card--star" />
-        <span>5.0</span>
-        <span className="gray">(6) • </span>
-        <span className="gray">USA</span>
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}) • </span>
+        <span className="gray">{props.country}</span>
       </div>
-      <h2>Life Lessons with Katie Zaferes</h2>
+      <h2>{props.title}</h2>
       <p>
-        <span className="bold">From $136</span> / person
+        <span className="bold">From ${props.price}</span> / person
       </p>
     </div>
   );
