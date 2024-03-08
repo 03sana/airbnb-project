ad8319cc4b049a9d1d0f114e31bfb1db38b25388
sana <sana.nasani3@gmail.com>
2024-03-08 20:01:38 +0300

    added key prop to Card comp

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,28 @@ import ReactDOM from "react-dom/client";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Card from "./components/Card.jsx";
+import data from "./data.jsx";
 
 export default function App() {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        key={item.id}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
+
   return (
     <div>
       <Navbar />
       <Hero />
-
-      <Card />
+      <section className="cards-list">{cards}</section>
     </div>
   );
 }
