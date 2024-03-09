66f9402034e443c1a7880fb32d403e63ec975d51
03coder <105085789+03sana@users.noreply.github.com>
2024-03-10 00:06:51 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/airbnb-project/",
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8000,
+  },
+  resolve: {},
+  base: "/airbnb-project/" 
 });
